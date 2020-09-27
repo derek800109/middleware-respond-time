458df863d45a06490e6c9cca1199f3e8de9a67b5
Derek Hsu <derek800109@gmail.com>
2020-09-27 19:41:39 +0800

    feat: add total ms into app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-// app.js
+let req_datetime
+
+// -----------------------------------------------------------------------------
+
 const express = require('express')
 
 // -----------------------------------------------------------------------------
@@ -8,42 +11,84 @@ const port = 3000
 
 // -----------------------------------------------------------------------------
 
-function get_now_datetime() {
-  const full_date = new Date(new Date().toUTCString())
-  const year = full_date.getFullYear()
-  const month = full_date.getMonth()
-  const day = full_date.getDate()
-  const hour = full_date.getHours()
-  const minute = full_date.getMinutes()
-  const second = full_date.getSeconds()
+function get_datetime_string(full_datetime) {
+  const utc_datetime = new Date(full_datetime.toUTCString())
+
+  const year = utc_datetime.getFullYear()
+  const month = utc_datetime.getMonth()
+  const day = utc_datetime.getDate()
+  const hour = utc_datetime.getHours()
+  const minute = utc_datetime.getMinutes()
+  const second = utc_datetime.getSeconds()
 
   return `${year}-${month}-${day} ${hour}:${minute}:${second}`
 }
 
+function get_datatime() {
+  // console.log('----------------------------', (new Date()).getTime())
+  return new Date()
+}
+
+function get_total_ms(req_datetime, res_datetime) {
+  const total_ms = res_datetime.getTime() - req_datetime.getTime()
+  // console.log(req_datetime.getTime(), res_datetime.getTime(), total_ms)
+
+  return total_ms
+}
+
+function server_log(method, url) {
+  // this part is for slow the request-response-cycle
+  console.log('slow down resquest-response-cycle...')
+  console.log('slow down resquest-response-cycle...')
+  console.log('slow down resquest-response-cycle...')
+
+  const res_datetime = get_datatime()
+  const total_ms = get_total_ms(req_datetime, res_datetime)
+  const daytime_ = get_datetime_string(req_datetime)
+
+  console.log(`${daytime_} | ${method} from ${url} | total time: ${total_ms}ms`)
+}
+
+// -----------------------------------------------------------------------------
+
 app.use(function (req, res, next) {
-  const day = get_now_datetime()
-  console.log(`${day} | ${req.method} from ${req.originalUrl}`)
+  req_datetime = get_datatime()
+  // const day = get_datetime_string(req_datetime)
+  // console.log(`${day} | ${req.method} from ${req.originalUrl}`)
   next()
 })
 
 // -----------------------------------------------------------------------------
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res, next) {
+  server_log(req.method, req.originalUrl)
   res.send('列出全部 Todo')
 })
 
 app.get('/new', (req, res) => {
+  server_log(req.method, req.originalUrl)
   res.send('新增 Todo 頁面')
 })
 
 app.get('/:id', (req, res) => {
+  server_log(req.method, req.originalUrl)
   res.send('顯示一筆 Todo')
 })
 
 app.post('/', (req, res) => {
+  server_log(req.method, req.originalUrl)
   res.send('新增一筆  Todo')
 })
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
+})
+
+
+// -----------------------------------------------------------------------------
+
+app.use(function (req, res, next) {
+  const day = get_now_datetime()
+  console.log(`after ${day} | ${req.method} from ${req.originalUrl}`)
+  next()
 })
